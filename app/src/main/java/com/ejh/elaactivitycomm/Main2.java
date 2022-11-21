76b0d3d7297c118d4602f62dff8aa8a8058662a6
elahoward <ehoward@tfs.ca>
2022-11-20 21:35:52 -0500

    j

--- app/src/main/java/com/ejh/elaactivitycomm/Main2.java
+++ app/src/main/java/com/ejh/elaactivitycomm/Main2.java
@@ -2,22 +2,38 @@ package com.ejh.elaactivitycomm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2 extends AppCompatActivity {
     private TextView textView;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         textView=findViewById(R.id.textView);
+        button2=findViewById(R.id.button2);
+
         Bundle extras=getIntent().getExtras();
         if (extras!=null){
          String message= extras.getString("msg1");
          textView.setText(message);
         }
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent =getIntent();
+                returnIntent.putExtra("returnData", "retour");
+                setResult(RESULT_OK,returnIntent);
+                finish();
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/ejh/elaactivitycomm/MainActivity.java
+++ app/src/main/java/com/ejh/elaactivitycomm/MainActivity.java
@@ -1,20 +1,25 @@
 package com.ejh.elaactivitycomm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private int REQUEST_CODE=2;
+    private TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
                 i.putExtra("msg1", "Hello");
                 i.putExtra("msg2", "Bonjour");
                 i.putExtra("msg2", 550202);
+                startActivityForResult(i, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_CODE){
+            if (requestCode==RESULT_OK){
+                String result= data.getStringExtra("returnData");
+                textView1.setText(result);
+            }
+        }
     }
 }
